755ca54f04097a3a80f644caf8b81460fd5363fb
rupert654 <rupert654@gmail.com>
2011-01-17 05:59:45 +0000

    Added configuration module with read function

---
+++ source/config/config.c
@@ -0,0 +1,56 @@
+#include "config.h"
+
+char *conf_read(char *filename, char *section, char *key) {
+  int   in_section = 0;
+  FILE  *conf_file;
+  char  line[MAX_CHARS], section_heading[MAX_CHARS] = "[", file_key[MAX_CHARS],
+        path[MAX_CHARS] = CONF_DIR;
+  char static value[MAX_CHARS];
+  
+  strcat(section_heading, section);
+  strcat(section_heading, "]");
+  
+  strcat(path, filename);
+  
+  conf_file = fopen(path, "r");
+  if(conf_file == NULL) {
+    perror("Error");
+    exit(1);
+  }
+  else {
+    while(fgets(line, MAX_CHARS, conf_file)) {
+      if(in_section) {
+        if(conf_is_section(line)) {
+          in_section = 0;
+        }
+        else {
+          sscanf(line, "%s %[^\t\n]", file_key, value);
+          if(strcmp(key, file_key) == 0) {
+            fclose(conf_file);
+            return value;
+          }
+        }
+      }
+      else if(conf_is_section(line) && strcmp(line, section_heading)) {
+        in_section = 1;
+      }
+    }
+  }
+  return "";
+}
+
+int conf_update(char* filename, char* section, char* key) {
+  //FILE* conf_file;
+  
+  //conf_file = fopen(CONF_DIR "/" filename, "w");
+  
+  return 0;
+}
+
+int conf_is_section(char line[MAX_CHARS]) {
+  if(line[0] == '[') {
+    return 1;
+  }
+  
+  return 0;
+}
---
+++ source/config/config.h
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#ifdef DEBUG
+  #define CONF_DIR "conf/"
+#else
+  #define CONF_DIR "path/to/usr/dir/"
+#endif
+
+#define MAX_CHARS 100
+
+char *conf_read(char *filename, char *section, char *key);
+int conf_update(char *filename, char *section, char *key);
+int conf_is_section(char line[MAX_CHARS]);
