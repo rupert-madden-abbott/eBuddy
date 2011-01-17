8a55b7d56007dda277d62ae7508e8f871774814a
Andrew Barber <andrew.ab@gmail.com>
2011-01-17 01:48:14 +0000

    gesture.c added ability to play sounds using mpg123

--- gestures/gestures.h
+++ gestures/gestures.h
@@ -1,3 +1,4 @@
+#include "eBuddy.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "phidget21.h"
@@ -13,15 +14,21 @@
 #define STARTPOS_ARM 60
 #define STARTPOS_ARM 60
 
-extern CPhidgetAdvancedServoHandle gestures_get_servo_handle(void);
+#define SETPOS(a, b, c) CPhidgetAdvancedServo_setPosition (a, b, c)
+#define SETENG(a, b, c) CPhidgetAdvancedServo_setEngaged(a, b, c);
+#define SETACC(a, b, c) CPhidgetAdvancedServo_setAcceleration(a, b, c)
+#define SETVEL(a, b, c) CPhidgetAdvancedServo_setVelocityLimit(a, b, c);
 
-int gestures_servo_DetachHandler(CPhidgetHandle phidget_servo, void *p);
+CPhidgetAdvancedServoHandle gestures_get_servo_handle(void);
+
+int gs_servo_DetachHandler(CPhidgetHandle phidget_servo, void *p);
 //int gestures_servo_AttachHandler(CPhidgetHandle phidget_servo, void *p);
-int gestures_servo_ErrorHandler(CPhidgetHandle phidget_servo, void *p, int ErrorCode, const char *Description);
+int gs_servo_ErrorHandler(CPhidgetHandle phidget_servo, void *p, int ErrorCode, const char *Description);
 
-int gestures_set_pos(CPhidgetAdvancedServoHandle servo);
-int gestures_dance_basic(void);
+int gs_pos(CPhidgetAdvancedServoHandle servo);
+extern int gs_dance_basic(void);
+extern int gs_sound(int sound, int itineration);
 
-CPhidgetAdvancedServoHandle gestures_servo_initialise(void);
+extern CPhidgetAdvancedServoHandle gs_servo_initialise(void);
 
-extern int testy(void);
+
