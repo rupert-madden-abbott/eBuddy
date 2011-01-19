fa5f3b3577d610f3ddfca7c2ec2377b64811ce9a
Andrew Barber <andrew.ab@gmail.com>
2011-01-19 12:14:50 +0000

    changed sleep to usleep in gestures.c

--- source/gestures/gestures.c
+++ source/gestures/gestures.c
@@ -16,7 +16,7 @@ int gs_set_pos(CPhidgetAdvancedServoHandle servo)
 	GS_SETENG(servo, GS_SERVO_TORSO, 1);
 	GS_SETENG(servo, GS_SERVO_LEFTARM, 1);
 	GS_SETENG(servo, GS_SERVO_RIGHTARM, 1);
-	sleep(1);
+	usleep(500);
 
 
 
@@ -48,25 +48,25 @@ int gs_dance_basic(CPhidgetAdvancedServoHandle servo)
 	for(i = 0; i < 2; i++){
 
 	GS_SETPOS (servo, GS_SERVO_LEFTARM, 100);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_LEFTARM, GS_STARTPOS_ARM);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_RIGHTARM, 100);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_RIGHTARM, GS_STARTPOS_ARM);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_HEAD, 100);
 	GS_SETPOS (servo, GS_SERVO_LEFTARM, 100);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_HEAD, GS_STARTPOS_HEAD);
 	GS_SETPOS (servo, GS_SERVO_LEFTARM, GS_STARTPOS_ARM);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_HEAD, 200);
 	GS_SETPOS (servo, GS_SERVO_RIGHTARM, 100);
-	sleep(1);
+	usleep(500);;
 	GS_SETPOS (servo, GS_SERVO_HEAD, GS_STARTPOS_HEAD);
 	GS_SETPOS (servo, GS_SERVO_LEFTARM, GS_STARTPOS_ARM);
-	sleep(1);
+	usleep(500);;
 	}
 
 
@@ -90,7 +90,7 @@ int gs_raise_arms(CPhidgetAdvancedServoHandle servo)
 	GS_SETENG(servo, GS_SERVO_RIGHTARM, 1);
     GS_SETPOS (servo, GS_SERVO_LEFTARM, 200);
     GS_SETPOS (servo, GS_SERVO_RIGHTARM, 200);
-    sleep(1);
+    usleep(500);;
     gs_set_pos(servo);
 
     return 0;
@@ -122,12 +122,12 @@ int gs_shake_head(CPhidgetAdvancedServoHandle servo)
 {
     GS_SETENG(servo, GS_SERVO_HEAD, 1);
     GS_SETPOS (servo, GS_SERVO_HEAD, 200);
-    sleep(1);
+    usleep(500);;
     GS_SETPOS (servo, GS_SERVO_HEAD, 20);
-    sleep(1);
+    usleep(500);;
     GS_SETPOS (servo, GS_SERVO_HEAD, 200);
     GS_SETPOS (servo, GS_SERVO_HEAD, 20);
-    sleep(1);
+    usleep(500);;
     gs_set_pos(servo);
 
     return 0;
@@ -139,15 +139,15 @@ int gs_move_arms(CPhidgetAdvancedServoHandle servo)
 	GS_SETENG(servo, GS_SERVO_RIGHTARM, 1);
     GS_SETPOS (servo, GS_SERVO_LEFTARM, 20);
     GS_SETPOS (servo, GS_SERVO_RIGHTARM, 200);
-    sleep(1);
+    usleep(500);;
     GS_SETPOS (servo, GS_SERVO_LEFTARM, 200);
     GS_SETPOS (servo, GS_SERVO_RIGHTARM, 20);
-    sleep(1);
+    usleep(500);;
     GS_SETPOS (servo, GS_SERVO_LEFTARM, 200);
     GS_SETPOS (servo, GS_SERVO_RIGHTARM, 20);
-    sleep(1);
+    usleep(500);;
     gs_set_pos(servo);
-    
+
     return 1;
 }
 
