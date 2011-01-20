dc3ed595361cbf6473b5570ad2d264b7f51777bb
Rowan Saundry <scragglez@gmail.com>
2011-01-20 15:11:08 +0000

    new main file

--- source/main/main.h
+++ source/main/main.h
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DEBUG 1
+
+#define ONE_MIN (60)
+#define ONE_HOUR (60*60)
+#define ONE_DAY (24*60*60)
+
 #ifdef __linux__
 #define OS "linux"
 #endif
@@ -8,12 +19,53 @@
 #endif
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+/* representation of a gesture [needs to go in gesture] */
+typedef struct ges_response {
+  void *gesture;	/* pointer to the gesture that should be called */
+  char *message;	/* string to display on the lcd screen */
+} ges_Response;
 
-#define DEBUG 1
+typedef enum error_code {
+  err_unknown = -1,	/* unknow error */ 
+  err_none = 0,		/* no error */
+  err_bad_path,		/* file path is invalid */
+  err_bad_file		/* file is corrupt */
+} error_code;
+
+/* represents the action to take when an input is recieved */
+typedef struct input_action {
+  em_Response primary_emotion;
+  em_Response secondary_emotion;
+  ges_Response full_gesture;
+  ges_Response normal_gesture;
+  ges_Response low_gesture;
+  int mode;
+} InputAction;
+
+/* represents the gesture to perform when an emotion reaches a certain level */
+typedef struct emotion_action {
+  ges_Response full_gesture;
+  ges_Response low_gesture;
+  ges_Response critical_gesture;
+  int mode;
+} EmotionAction;
+
+/* list of emotions and their decay times etc */
+const em_Emotion emotions = {
+  {24,	0.5,	10,	800,	200,	100},	/* hungar */
+  {}
+};
+
+/* list of reactions for each emoton */
+const EmotionAction emotion_actions[] = {
+  {{NULL,	""}, {NULL,	""}, {NULL,	""},	0}		/* hunger */
+}
+
+const InputAction input_actions[] = {
+  {{0,	0,	0},	{0,	0,	0}, {NULL,	""}, {NULL,	""}, {NULL,	""},	0}	/* somthing happens */
+
+}
+
 
 void end(void);
 int main_loop(void);
