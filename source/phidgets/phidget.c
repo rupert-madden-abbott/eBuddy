a7f3375aaa7ef704676dc211f7b55ccb3433bba1
Ahmed Sarwat <engahmedmail@gmail.com>
2011-01-20 12:24:06 +0000

    Adding RFID functions

--- source/phidgets/phidget.c
+++ source/phidgets/phidget.c
@@ -7,11 +7,14 @@ int ph_initialise(void)
 {
     ph_get_servo_handle();
 
+    ph_get_RFID_handle();	
+
     return 0;
 }
 
 int ph_destruct(void)
 {
+    ph_RFID_closerfid();
     return 0;
 }
 
@@ -92,4 +95,141 @@ int ph_servo_ErrorHandler(CPhidgetHandle phidget_servo, void *p, int ErrorCode, 
 
 	return 0;
 }
+
+/*RFID*/
+
+CPhidgetRFIDHandle ph_get_RFID_handle (void)
+{
+	static int RFID_initialised=0;
+	static CPhidgetRFIDHandle rfid =0;
+	if(RFID_initialised ==0)
+		{
+			RFID_initialised=1;
+			rfid=ph_RFID_openrfid();
+			
+		}
+	return rfid;
+}
+
+CPhidgetRFIDHandle ph_RFID_openrfid(void)
+{
+int static result;
+const char *err;
+CPhidgetRFIDHandle rfid =0;
+CPhidgetRFID_create(&rfid);
+
+CPhidget_set_OnAttach_Handler((CPhidgetHandle)rfid, ph_RFID_AttachHandler, NULL);
+CPhidget_set_OnDetach_Handler((CPhidgetHandle)rfid, ph_RFID_DetachHandler, NULL);
+CPhidget_set_OnError_Handler((CPhidgetHandle)rfid, ph_RFID_ErrorHandler, NULL);
+
+CPhidgetRFID_set_OnOutputChange_Handler(rfid, ph_RFID_OutputChangeHandler, NULL);
+
+CPhidgetRFID_set_OnTag_Handler(rfid, ph_RFID_TagHandler, NULL);
+CPhidgetRFID_set_OnTagLost_Handler(rfid, ph_RFID_TagLostHandler, NULL);
+
+CPhidget_open((CPhidgetHandle)rfid, -1);
+
+//get the program to wait for an RFID device to be attached
+if((result = CPhidget_waitForAttachment((CPhidgetHandle)rfid, 10000)))
+	{
+		CPhidget_getErrorDescription(result, &err);
+		printf("Error ebuddy RFID not connected: %s\n", err);
+		ph_RFID_closerfid();
+		exit (1);
+	}
+CPhidgetRFID_setAntennaOn(rfid, 1);
+return rfid;
+}
+
+void ph_RFID_closerfid(void)
+{
+CPhidgetRFIDHandle rfid;
+rfid=ph_get_RFID_handle();
+CPhidget_close((CPhidgetHandle)rfid);
+CPhidget_delete((CPhidgetHandle)rfid);
+}
+
+
+int ph_RFID_AttachHandler(CPhidgetHandle RFID, void *userptr)
+{
+	int serialNo;
+	const char *name;
+
+	CPhidget_getDeviceName (RFID, &name);
+	CPhidget_getSerialNumber(RFID, &serialNo);
+	printf("ebuddy RFID attached\n");
+
+	return 0;
+}
+
+int ph_RFID_DetachHandler(CPhidgetHandle RFID, void *userptr)
+{
+	int serialNo;
+	const char *name;
+
+	CPhidget_getDeviceName (RFID, &name);
+	CPhidget_getSerialNumber(RFID, &serialNo);
+	printf("ebuddy RFID detached!\n");
+
+	return 0;
+}
+
+int ph_RFID_ErrorHandler(CPhidgetHandle RFID, void *userptr, int ErrorCode, const char *unknown)
+{
+	printf("ebuddy RFID error %d - %s\n", ErrorCode, unknown);
+	return 0;
+}
+
+
+int ph_RFID_TagHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal)
+{
+	//turn on the Onboard LED
+	CPhidgetRFID_setLEDOn(RFID, 1);
+
+	
+	printf("Tag Read: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1], TagVal[2], TagVal[3], TagVal[4]);
+	ph_RFID_savetag(TagVal[0]+TagVal[1]+TagVal[2]+TagVal[3]+TagVal[4]);
+	return 0;
+}
+
+int ph_RFID_TagLostHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal)
+{
+	//turn off the Onboard LED
+	CPhidgetRFID_setLEDOn(RFID, 0);
+
+	printf("Tag Lost: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1], TagVal[2], TagVal[3], TagVal[4]);
+	return 0;
+}
+
+int ph_RFID_OutputChangeHandler(CPhidgetRFIDHandle RFID, void *usrptr, int Index, int State)
+{
+	if(Index == 0 || Index == 1)
+	{
+		//printf("Output: %d > State: %d\n", Index, State);
+	}
+	return 0;
+}
+
+void ph_RFID_food(void)
+{
+	printf("I am eating\n");
+}
+void ph_RFID_drink(void)
+{
+	printf("I am drinking\n");
+}
+void ph_RFID_savetag(int tagv)
+{
+
+if(tagv==386)
+	{
+		ph_RFID_food();
+	}
+if(tagv==601)
+	{
+		ph_RFID_drink();
+	}
+
+}
+
 
