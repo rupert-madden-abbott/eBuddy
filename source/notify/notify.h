c21241e624b2340f1317418b5ab8508389e8b775
rupert654 <rupert654@gmail.com>
2011-01-23 01:15:35 +0000

    Futher notification improvements

--- source/notify/notify.h
+++ source/notify/notify.h
@@ -6,25 +6,44 @@
 #include <pthread.h>
 #include <oauth.h>
 #include <string.h>
+#include <ctype.h>
+#include <curl/curl.h>
 
 #define NOTI_CONF_FILE "notify.conf"
 
-#define NOTI_TWITTER_KEY "pFQbE7wR2YXfGbp8muYKA"
-#define NOTI_TWITTER_SECRET "iam3544JfaQLjS13pO6LjsGWtDtpanZIXrB93vnk4"
 #define NOTI_TWITTER_REQUEST "https://api.twitter.com/oauth/request_token"
 #define NOTI_TWITTER_ACCESS "https://api.twitter.com/oauth/access_token"
 #define NOTI_TWITTER_AUTHORIZE "https://api.twitter.com/oauth/authorize"
 
+#define NOTI_MAX 200
+
 typedef struct noti_token {
-	char key[50];
-	char secret[50];
+	char key[NOTI_MAX];
+	char secret[NOTI_MAX];
 } noti_token;
 
+struct MemoryStruct {
+  char *data;
+  size_t size; //< bytes remaining (r), bytes accumulated (w)
+};
+
 int noti_initialize(void);
-int noti_authenticate(noti_token *user);
+int noti_destroy(void);
+int noti_authenticate(noti_token app, noti_token *user, char *config);
 int noti_request_token(char *uri, noti_token app, noti_token *user);
 int noti_parse_response(char *response, noti_token *token);
-int noti_read_arg(char *arg, char *type, char *value);
+int noti_parse_arg(char *arg, char *type, char *value);
+int noti_poll(char *uri, noti_token app, noti_token user);
+char *noti_curl_get (const char *uri, const char *query);
+size_t WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data);
+
+
+int noti_isyes(char input);
+int noti_isno(char input);
+int noti_isans(char input);
+int noti_ask(char *question, char *input);
+int noti_flush(void);
+int noti_validate_int(char *line);
 
 #endif
 
