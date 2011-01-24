6d7513d74b5d23f5095818c9f14596809d2a35b3
Ahmed Sarwat <engahmedmail@gmail.com>
2011-01-24 16:33:26 +0000

    creating input folder and input.c and .h files

---
+++ source/input/input.c
@@ -0,0 +1,35 @@
+/* RFID input*/
+int in_RFID_TagHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal)
+{
+	//turn on the Onboard LED
+	CPhidgetRFID_setLEDOn(RFID, 1);
+
+	
+	printf("Tag Read: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1], TagVal[2], TagVal[3], TagVal[4]);
+	ph_RFID_savetag(TagVal[0]+TagVal[1]+TagVal[2]+TagVal[3]+TagVal[4]);
+	return 0;
+}
+
+
+int in_RFID_TagLostHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal)
+{
+	//turn off the Onboard LED
+	CPhidgetRFID_setLEDOn(RFID, 0);
+
+	printf("Tag Lost: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1], TagVal[2], TagVal[3], TagVal[4]);
+	return 0;
+}
+
+/*Interface Kit input*/
+int in_kit_InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State)
+{
+	//printf("Digital Input: %d > State: %d\n", Index, State);
+	return 0;
+}
+
+int in_kit_SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int Value)
+{
+	//printf("Sensor: %d > Value: %d\n", Index, Value);
+	ph_kit_laugh(Index,Value);
+	return 0;
+}
---
+++ source/input/input.h
@@ -0,0 +1,6 @@
+/* RFID input header*/
+int in_RFID_TagHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal);
+int in_RFID_TagLostHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal);
+/*Interface Kit input header*/
+int in_kit_InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State);
+int in_kit_SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int Value);
