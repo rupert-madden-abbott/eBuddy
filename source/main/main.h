b2b99588b56bc3638e32687f0efbdf2e7c41ceba
rupert654 <rupert654@gmail.com>
2011-01-25 01:27:50 +0000

    Moved parts of main.h to utilities.h

--- source/main/main.h
+++ source/main/main.h
@@ -1,13 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "emotion.h"
+#include "gesture_interface.h"
+#include "notify.h"
+
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <jansson.h>
-
-#define DEBUG 1
+#include "notify.h"
+#include "phidget.h"
+#include "gesture_interface.h"
+#include "emotion.h"
+#include "config.h"*/
 
 #define CONFIG_PATH "conf/main.json"
 
@@ -15,44 +23,21 @@
 #define ONE_HOUR (60*60)
 #define ONE_DAY (24*60*60)
 
-#ifdef __linux__
-#define OS LINUX
-#endif
-#ifndef NeXTBSD
-#ifdef __APPLE__
-#define OS OSX
-#endif
-#endif
-
-/**
- * \todo Placeholder for em_Response needs to be filled out
- */
-typedef struct em_response {
-  void *example; /* This needs to be filled out */
-} em_Response;
-
-typedef enum error_code {
-  err_unknown = -1,	/* unknow error */
-  err_none = 0,		/* no error */
-  err_bad_path,		/* file path is invalid */
-  err_bad_file		/* file is corrupt */
-} error_code;
-
 /* represents the action to take when an input is recieved */
 typedef struct input_action {
   em_Response primary_emotion;
   em_Response secondary_emotion;
-  ges_Response full_gesture;
-  ges_Response normal_gesture;
-  ges_Response low_gesture;
+  gsi_Response full_gesture;
+  gsi_Response normal_gesture;
+  gsi_Response low_gesture;
   int mode;
 } InputAction;
 
 /* represents the gesture to perform when an emotion reaches a certain level */
 typedef struct emotion_action {
-  ges_Response full_gesture;
-  ges_Response low_gesture;
-  ges_Response critical_gesture;
+  gsi_Response full_gesture;
+  gsi_Response low_gesture;
+  gsi_Response critical_gesture;
   int mode;
 } EmotionAction;
 
@@ -86,8 +71,6 @@ const InputAction input_actions[] = {
 
 };
 #endif
-
-enum os { LINUX, OSX, WINDOWS };
 
 void end(void);
 int main_loop(void);
