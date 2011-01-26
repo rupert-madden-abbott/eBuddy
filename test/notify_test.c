681ae4c6826e899dd85df9d728dc2cabf2c1bf0a
rupert654 <rupert654@gmail.com>
2011-01-26 14:31:52 +0000

    Modified queue and notify tests

--- test/notify_test.c
+++ test/notify_test.c
@@ -8,11 +8,25 @@
 #include <jansson.h>
 #include "utility.h"
 #include "config.h"
+#include "queue.h"
 #include "notify.h"
 #include "test.h"
 
 int main(void) {
+  qu_queue *queue = qu_init();
+  pthread_t thread;
+  nt_message *msg;
 
-
-  return 0;
+  pthread_create(&thread, NULL, nt_poll, queue);
+  
+  while(1) {
+    sleep(1);
+    if(qu_size(queue) > 0) {
+      msg = qu_pop(queue);
+      printf("%s", msg->text);
+      fflush(stdout);
+    }
+  }
+ 
+  pthread_exit(NULL);
 }
--- test/queue_test.c
+++ test/queue_test.c
@@ -2,26 +2,47 @@
  * @file   queue_test.c
  * @author Rupert Madden-Abbott
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "utility.h"
 #include "queue.h"
 #include "test.h"
 
+void *loop(void *queue);
+
 int main(void) {
-  qu_queue *queue = NULL;
-  char     *first = "hello";
-  char     *second = "goodbye";
-  
+  qu_queue   *queue = NULL;
+  pthread_t thread;
+
   queue = qu_init();
   if(!queue) fail("Failed to create new queue");
 
-  qu_push(queue, first);
-  qu_push(queue, second);
-    qu_free(queue);
+  pthread_create(&thread, NULL, loop, queue);
+
   
-  qu_pop(queue);
-  /*printf("%s", (char *) qu_pop(queue));
+  while(1) {
+    sleep(1);
     printf("%s", (char *) qu_pop(queue));
-  */
+    fflush(stdout);
+  }
 
+  qu_free(queue);
 
-  return 0;
+  pthread_exit(NULL);
+}
+
+void *loop(void *queue) {
+  char first[] = "hello";
+  char second[] = "goodbye";
+  
+  while(1) {
+    sleep(1);
+    qu_push(queue, first);
+    qu_push(queue, second);
+  }
+  
+  pthread_exit(NULL);
 }
