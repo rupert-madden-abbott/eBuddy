47a0e5a835c4ad2de78cd91db25abecf27271842
Rowan Saundry <scragglez@gmail.com>
2011-01-27 18:41:14 +0000

    added more error codes

--- source/utility.h
+++ source/utility.h
@@ -22,11 +22,13 @@ enum os {
 };
 
 typedef enum error_code {
-  ERR_UNKNOWN = -1,	/* unknown error */ 
-  ERR_NONE = 0,		/* no error */
-  ERR_BAD_PATH,		/* file path is invalid */
-  ERR_BAD_FILE,		/* file is corrupt */
-  ERR_BAD_ARG
+  ERR_UNKNOWN = -1,	//unknown error
+  ERR_NONE = 0,		//no error
+  ERR_BAD_PATH,		//file path is invalid
+  ERR_BAD_FILE,		//file is corrupt
+  ERR_BAD_ARG,		//invalid argument to a function
+  ERR_BAD_MODE,		//invalid run mode selected
+  ERR_BAD_ACTION	//an invalid action was requested
 } error_code;
 
 /* test to see if a path is valid and writable */
