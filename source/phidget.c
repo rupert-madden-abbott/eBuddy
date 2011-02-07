d241d523bf35cd8fd7e8624bc2834a1ec42d94b3
Ahmed Sarwat <engahmedmail@gmail.com>
2011-02-07 17:37:30 +0000

    limiting char/line in phidget.h and phidget.c

--- source/phidget.c
+++ source/phidget.c
@@ -30,13 +30,17 @@ int ph_servo_init(ph_handle *handle)
     const char *servo_attach_error;
     handle->servohandle = 0;
     CPhidgetAdvancedServo_create(&handle->servohandle);
-    CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->servohandle, ph_servo_AttachHandler, NULL);
-    CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->servohandle, ph_servo_DetachHandler, NULL);
-    CPhidget_set_OnError_Handler((CPhidgetHandle)handle->servohandle, ph_servo_ErrorHandler, NULL);
+    CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->servohandle,
+                                  ph_servo_AttachHandler, NULL);
+    CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->servohandle,
+                                  ph_servo_DetachHandler, NULL);
+    CPhidget_set_OnError_Handler((CPhidgetHandle)handle->servohandle,
+                                 ph_servo_ErrorHandler, NULL);
 
     CPhidget_open((CPhidgetHandle)handle->servohandle, -1);
 
-    if((servo_wait_result = CPhidget_waitForAttachment((CPhidgetHandle)handle->servohandle, 10000)))
+    if((servo_wait_result = CPhidget_waitForAttachment((CPhidgetHandle)handle->
+                                                       servohandle, 10000)))
     {
         CPhidget_getErrorDescription(servo_wait_result, &servo_attach_error);
         printf("Error eBuddy servo not connected: %s\n", servo_attach_error);
@@ -65,7 +69,8 @@ int ph_servo_AttachHandler(CPhidgetHandle phidget_servo, void *p)
 	return 0;
 }
 
-int ph_servo_ErrorHandler(CPhidgetHandle phidget_servo, void *p, int ErrorCode, const char *Description)
+int ph_servo_ErrorHandler(CPhidgetHandle phidget_servo, void *p,
+                          int ErrorCode, const char *Description)
 {
 
     printf("eBuddy servo error: %d %s\n", ErrorCode, Description);
@@ -90,16 +95,20 @@ const char *err;
 handle->RFIDhandle =0;
 CPhidgetRFID_create(&handle->RFIDhandle);
 
-CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->RFIDhandle, ph_RFID_AttachHandler, NULL);
-CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->RFIDhandle, ph_RFID_DetachHandler, NULL);
-CPhidget_set_OnError_Handler((CPhidgetHandle)handle->RFIDhandle, ph_RFID_ErrorHandler, NULL);
+CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->RFIDhandle, 
+                              ph_RFID_AttachHandler, NULL);
+CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->RFIDhandle,
+                              ph_RFID_DetachHandler, NULL);
+CPhidget_set_OnError_Handler((CPhidgetHandle)handle->RFIDhandle, 
+                              ph_RFID_ErrorHandler, NULL);
 
 CPhidget_open((CPhidgetHandle)handle->RFIDhandle, -1);
 
 //CPhidget_open((CPhidgetHandle)rfid, -1);
 
 //get the program to wait for an RFID device to be attached
-if((result = CPhidget_waitForAttachment((CPhidgetHandle)handle->RFIDhandle, 10000)))
+if((result = CPhidget_waitForAttachment((CPhidgetHandle)handle->RFIDhandle, 
+                                        10000)))
 	{
 		CPhidget_getErrorDescription(result, &err);
 		printf("Error ebuddy RFID not connected: %s\n", err);
@@ -142,7 +151,8 @@ int ph_RFID_DetachHandler(CPhidgetHandle RFID, void *userptr)
 	return 0;
 }
 
-int ph_RFID_ErrorHandler(CPhidgetHandle RFID, void *userptr, int ErrorCode, const char *unknown)
+int ph_RFID_ErrorHandler(CPhidgetHandle RFID, void *userptr, int ErrorCode,
+                         const char *unknown)
 {
 	//printf("eBuddy RFID error %d - %s\n", ErrorCode, unknown);
 	return 0;
@@ -159,12 +169,16 @@ int ph_kit_init(ph_handle *handle)
 	handle->IFKhandle = 0;
 	CPhidgetInterfaceKit_create(&handle->IFKhandle);
 
-	CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->IFKhandle, ph_kit_AttachHandler, NULL);
-	CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->IFKhandle, ph_kit_DetachHandler, NULL);
-	CPhidget_set_OnError_Handler((CPhidgetHandle)handle->IFKhandle, ph_kit_ErrorHandler, NULL);
+	CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->IFKhandle,
+                                      ph_kit_AttachHandler, NULL);
+	CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->IFKhandle,
+                                      ph_kit_DetachHandler, NULL);
+	CPhidget_set_OnError_Handler((CPhidgetHandle)handle->IFKhandle,
+                                     ph_kit_ErrorHandler, NULL);
 	CPhidget_open((CPhidgetHandle)handle->IFKhandle, -1);
 
-	if((result = CPhidget_waitForAttachment((CPhidgetHandle)handle->IFKhandle, 10000)))
+	if((result = CPhidget_waitForAttachment((CPhidgetHandle)handle->
+                                                IFKhandle,10000)))
 	{
 		CPhidget_getErrorDescription(result, &err);
 		printf("eBuddy interface kit not connected: %s\n", err);
@@ -208,9 +222,10 @@ int ph_kit_DetachHandler(CPhidgetHandle IFK, void *userptr)
 	return 0;
 }
 
-int ph_kit_ErrorHandler(CPhidgetHandle IFK, void *userptr, int ErrorCode, const char *unknown)
+int ph_kit_ErrorHandler(CPhidgetHandle IFK, void *userptr, int ErrorCode,
+                        const char *unknown)
 {
-	//printf("ebuddy interface kit Error handled. %d - %s", ErrorCode, unknown);
+//printf("ebuddy interface kit Error handled. %d - %s",ErrorCode, unknown);
 	return 0;
 }
 
@@ -230,16 +245,21 @@ int ph_lcd_init(ph_handle *handle)
 	//create the TextLCD object
 	CPhidgetTextLCD_create(&handle->LCDhandle);
 
-	//Set the handlers to be run when the device is plugged in or opened from software, unplugged or closed from software, or generates an error.
-	CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->LCDhandle, ph_lcd_AttachHandler, NULL);
-	CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->LCDhandle, ph_lcd_DetachHandler, NULL);
-	CPhidget_set_OnError_Handler((CPhidgetHandle)handle->LCDhandle, ph_lcd_ErrorHandler, NULL);
+//Set the handlers to be run when the device is plugged in or opened from
+//software, unplugged or closed from software, or generates an error.
+	CPhidget_set_OnAttach_Handler((CPhidgetHandle)handle->LCDhandle,
+                                      ph_lcd_AttachHandler, NULL);
+	CPhidget_set_OnDetach_Handler((CPhidgetHandle)handle->LCDhandle,
+                                      ph_lcd_DetachHandler, NULL);
+	CPhidget_set_OnError_Handler((CPhidgetHandle)handle->LCDhandle,
+                                     ph_lcd_ErrorHandler, NULL);
 
 	//open the TextLCD for device connections
 	CPhidget_open((CPhidgetHandle)handle->LCDhandle, -1);
 
 	//get the program to wait for an TextLCD device to be attached
-	if((result = CPhidget_waitForAttachment((CPhidgetHandle)handle->LCDhandle, 5000)))
+	if((result = CPhidget_waitForAttachment((CPhidgetHandle)handle->
+                                                LCDhandle, 5000)))
 	{
 		CPhidget_getErrorDescription(result, &err);
 		printf("Problem waiting for attachment: %s\n", err);
@@ -267,7 +287,8 @@ int ph_lcd_DetachHandler(CPhidgetHandle TXT, void *userptr)
 	return 0;
 }
 
-int ph_lcd_ErrorHandler(CPhidgetHandle TXT, void *userptr, int ErrorCode, const char *Description)
+int ph_lcd_ErrorHandler(CPhidgetHandle TXT, void *userptr, int ErrorCode,
+                        const char *Description)
 {
 	printf("Error handled. %d - %s\n", ErrorCode, Description);
 	return 0;
