4c2881b8bb8211ca53462bc8dfe8d1de15af4efa
TheWire <andrew.ab@gmail.com>
2011-02-07 17:05:31 +0000

    changed style in gs and gsi added doxygen comments

--- source/gesture.h
+++ source/gesture.h
@@ -1,3 +1,14 @@
+/**
+ *@file gesture.h
+ *@author Andrew Barber & Andrew Pickin (LCD code)
+ *
+ *The gesture module contains all output to the phidgets made via calls to the
+ *phidget library.  All functions take an argument of the relevant 
+ *CPhidgetHandle and return a phidget error code greater than zero.  This
+ *module also contains code to play sounds. 
+ */
+
+
 #ifndef GESTURES_H
 #define GESTURES_H
 
@@ -16,55 +27,122 @@
 #define SCROLLSPEED 1500000
 #define WHITESPACE 32  // ascii code for whitespace
 
-//servo mappings
+
+///{
+/**
+ *@define GS_SERVO_
+ *specifies servo mappings i.e. physical layout of servos on the controller
+ */
 #define GS_SERVO_HEAD 0
 #define GS_SERVO_TORSO 1
 #define GS_SERVO_LEFTARM 2
 #define GS_SERVO_RIGHTARM 3
+///}
 
-//default servo postions
+///{
+/**
+ *@define GS_STARTPOS_
+ *default postions of servos
+ */
 #define GS_STARTPOS_HEAD 110
 #define GS_STARTPOS_TORSO 110
 #define GS_STARTPOS_RIGHTARM 200
 #define GS_STARTPOS_LEFTARM 20
+///}
 
-
-//defined servo phidget library fucntions
-#define GS_SETPOS(a, b, c) if((error = CPhidgetAdvancedServo_setPosition (a, b, c)) != 0) { \
-                             return error; \
-                           }
-#define GS_SETENG(a, b, c) if((error = CPhidgetAdvancedServo_setEngaged(a, b, c)) != 0) { \
-                             return error; \
-                           }
-#define GS_SETACC(a, b, c) if((error = CPhidgetAdvancedServo_setAcceleration(a, b, c)) != 0) { \
-                             return error; \
-                           }
-#define GS_SETVEL(a, b, c) if((error = CPhidgetAdvancedServo_setVelocityLimit(a, b, c)) != 0) { \
-                             return error; \
-                           }
-
+///{
+/**
+ *@define GS_*EYE
+ *LED mapping for the eyes
+ */
 #define GS_RIGHTEYE 0
 #define GS_LEFTEYE 2
+///}
+
+///{
+/**
+ *@define GS_SET*
+ *defined code to aid readability in long getsure functions.
+ *@a CPhidgetAdvancedServoHandle
+ *@b servo index
+ *@c position or state
+ */
+
+#define GS_SETPOS(a, b, c) if((error = CPhidgetAdvancedServo_setPosition \
+                             (a, b, c)) != 0) { \
+                             return error; \
+                           }
+#define GS_SETENG(a, b, c) if((error = CPhidgetAdvancedServo_setEngaged \
+                             (a, b, c)) != 0) { \
+                             return error; \
+                           }
+#define GS_SETACC(a, b, c) if((error = CPhidgetAdvancedServo_setAcceleration \
+                             (a, b, c)) != 0) { \
+                             return error; \
+                           }
+#define GS_SETVEL(a, b, c) if((error = CPhidgetAdvancedServo_setVelocityLimit \
+                             (a, b, c)) != 0) { \
+                             return error; \
+                           }
+
+///}
 
 
-CPhidgetAdvancedServoHandle gs_get_servo_handle(void);
+///{
+/**
+ *These servo functions all act to create movements in the servos via directy
+ * calls to the phidget library.
+ *
+ *@arg CPhidgetAdvancedServoHandle servo This is the servo handle.
+ *
+ *
+ *@return int phidget library function call return code.
+*/
 
+/**
+ *function should be called after every gesture to reset sevro positions to default.
+ *Sets servo enaged status to 0 (off)
+ */
 int gs_set_pos(CPhidgetAdvancedServoHandle servo);
-extern int gs_dance_basic(CPhidgetAdvancedServoHandle servo);
+int gs_dance_basic(CPhidgetAdvancedServoHandle servo);
+int gs_dance_advanced(CPhidgetAdvancedServoHandle servo);
+int gs_raise_arms(CPhidgetAdvancedServoHandle servo);
+int gs_shake_head(CPhidgetAdvancedServoHandle servo);
+int gs_move_arms(CPhidgetAdvancedServoHandle servo);
+int gs_turn(CPhidgetAdvancedServoHandle servo);
+int gs_wave_left(CPhidgetAdvancedServoHandle servo);
+int gs_wave_right(CPhidgetAdvancedServoHandle servo);
+int gs_rapid_eyeflash(CPhidgetInterfaceKitHandle ifkit);
+int gs_raise_right(CPhidgetAdvancedServoHandle servo);
+int gs_raise_left(CPhidgetAdvancedServoHandle servo);
+
+///}
+
+
+///{
+/**
+ *These functions control the phidget LEDs via interface kit calls to the 
+ *phidget library.  Thse functions turn the eyes on and off. 
+ *
+ *@arg CPhidgetInterfaceKitHandle ifkit This is the interface kit handle.
+ *
+ *
+ *@return int phidget library function call return code.
+ */
+int gs_eyeson(CPhidgetInterfaceKitHandle ifkit);
+int gs_eyesoff(CPhidgetInterfaceKitHandle ifkit);
+int gs_eyeflash(CPhidgetInterfaceKitHandle ifkit);
+
+///}
+
+/**
+ *This function allows playing of sound via an external program
+ *
+ *@arg const char *sound The sound name specified in the sound.json file.
+ *@arg int repeat Allows the sound fucntion to repeat a sound multiple times.
+ *@return value greater than zero if error.
+ */
 int gs_sound(const char *sound, int repeat);
-extern int gs_dance_advanced(CPhidgetAdvancedServoHandle servo);
-extern int gs_raise_arms(CPhidgetAdvancedServoHandle servo);
-extern int gs_shake_head(CPhidgetAdvancedServoHandle servo);
-extern int gs_move_arms(CPhidgetAdvancedServoHandle servo);
-extern int gs_turn(CPhidgetAdvancedServoHandle servo);
-extern int gs_eyeson(CPhidgetInterfaceKitHandle ifkit);
-extern int gs_eyesoff(CPhidgetInterfaceKitHandle ifkit);
-extern int gs_eyeflash(CPhidgetInterfaceKitHandle ifkit);
-extern int gs_wave_left(CPhidgetAdvancedServoHandle servo);
-extern int gs_wave_right(CPhidgetAdvancedServoHandle servo);
-extern int gs_rapid_eyeflash(CPhidgetInterfaceKitHandle ifkit);
-extern int gs_raise_right(CPhidgetAdvancedServoHandle servo);
-extern int gs_raise_left(CPhidgetAdvancedServoHandle servo);
 
 /*
  * Takes a string as input, along with the LCD handle, and prints the string
--- source/gesture_interface.h
+++ source/gesture_interface.h
@@ -1,3 +1,15 @@
+/**
+ *@file gesture_interface.h
+ *@author Andrew Barber
+ *This module acts to interface between main functions and the gesture module.
+ *Funtions in this module take a ph_handle struct as an argument and return an
+ *error.
+ *This module allows the mapping of multiple gestures to a single interface
+ *call.
+ *This module also contains initialisation for the phidget output.
+ */
+
+
 #ifndef GESTURE_INTERFACE_H
 #define GESTURE_INTERFACE_H
 
@@ -8,9 +20,40 @@
 #include "gesture.h"
 #include "phidget.h"
 
-#define GSI_NULL 2
-#define GSI_PHER 1
-#define GSI_OK 0
+/**
+ *@enum GSI_*error
+ *gsi_ error codes
+ */
+typedef enum gsi_error {
+ GSI_OK = 0,
+ GSI_PHER =  1,
+ GSI_NULL = 2
+} gsi_error;
+
+/**
+ *@struct used by gsi_react function to determine function to call and pass correct value.
+ *@member int (*gesture)(ph_handle *) pointer to the gesture that should be called. 
+ *@member const char *message string to display on the lcd screen. 
+ *@member const char *sound Sound to be played.
+ *@member int sndrepeat SOund repeat.    
+*/
+
+typedef struct gsi_reaction {
+  int (*gesture)(ph_handle *);	
+  const char *message;        
+  const char *sound;
+  int sndrepeat;
+	
+} gsi_Reaction;
+
+
+///{
+/**
+ *These are standard gsi_ functions that map to one or more gestures.
+ *
+ *@arg ph_handle *handle Struct points to the handles for each phidget.
+ *@return GSI error code.
+ */
 
 int gsi_gesture_init(ph_handle *handle);
 int gsi_gesture_close(ph_handle *handle);
@@ -22,22 +65,39 @@ int gsi_move_arms(ph_handle *handle);
 int gsi_raise_arms(ph_handle *handle);
 int gsi_eyeflash(ph_handle *handle);
 int gsi_rapid_eyeflash(ph_handle *handle);
-int gsi_sound(const char *sound, int repeat);
 int gsi_raise_left(ph_handle *handle);
 int gsi_raise_right(ph_handle *handle);
 int gsi_wave_left(ph_handle *handle);
 int gsi_wave_right(ph_handle *handle);
 
-extern int gsi_printLCD(const char* str, ph_handle *handle);
+///}
 
+/**
+ *This function allows playing of sound via an external program
+ *
+ *@arg const char *sound The sound name specified in the sound.json file.
+ *@arg int repeat Allows the sound fucntion to repeat a sound multiple times.
+ *@return value greater than zero if error.
+ */
+int gsi_sound(const char *sound, int repeat);
 
-typedef struct gsi_reaction {
-    int (*gesture)(ph_handle *);	/* pointer to the gesture that should be called */
-    const char *message;        /* string to display on the lcd screen */
-    const char *sound;
-    int sndrepeat;
-	
-} gsi_Reaction;
+/** 
+ *This fucntion facilitates printing on the LCD screen.
+ *
+ *@arg const char* str String to be printed on the screen.
+ *@arg ph_handle *handle Struct points to the handles for each phidget.
+ *@return error code.
+ */ 
+int gsi_printLCD(const char* str, ph_handle *handle);
+
+/**
+ *Function handles calling of gsi_ functions incuding lcd text, sounds and
+ *gestures. 
+ *
+ *@arg const gsi_Reaction *resp Pointer containing fcuntions with arguments.
+ *@arg ph_handle *handle Struct points to the handles for each phidget.
+ *@return GSI error code.
+*/
 
 int gsi_react(const gsi_Reaction *resp, ph_handle *handle);
 #endif
