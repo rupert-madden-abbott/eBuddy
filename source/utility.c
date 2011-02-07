5a6cc98684f0842e528547cc382d0544ac897530
rupert654 <rupert654@gmail.com>
2011-02-07 23:39:58 +0000

    fixed compile errors in utility

--- source/utility.c
+++ source/utility.c
@@ -1,12 +1,9 @@
+#include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include "utility.h"
 
-<<<<<<< HEAD
-
 //test to see if a path is valid and writable
-=======
->>>>>>> Commented part of the utility module
 int ut_test_path(const char *path) {
   FILE *file;
 	
