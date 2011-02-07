449c965c0c4ccc8b1722bc389758e5c438968140
Ahmed Sarwat <engahmedmail@gmail.com>
2011-02-07 17:24:24 +0000

    adding comments and limit the char/line

--- source/input.c
+++ source/input.c
@@ -20,30 +20,38 @@ int in_RFID_init(CPhidgetRFIDHandle RFID)
 
 int in_kit_input_init(CPhidgetInterfaceKitHandle ifKit)
 {
-    CPhidgetInterfaceKit_set_OnInputChange_Handler (ifKit, in_kit_InputChangeHandler, NULL);
-    CPhidgetInterfaceKit_set_OnSensorChange_Handler (ifKit, in_kit_SensorChangeHandler, NULL);
+    CPhidgetInterfaceKit_set_OnInputChange_Handler (ifKit,
+                                                    in_kit_InputChangeHandler,
+                                                    NULL);
+    CPhidgetInterfaceKit_set_OnSensorChange_Handler (ifKit,
+                                                     in_kit_SensorChangeHandler,
+                                                     NULL);
     return 0;
 }
 
 int in_last_input;
 /* RFID input*/
-int in_RFID_TagHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal)
+int in_RFID_TagHandler(CPhidgetRFIDHandle RFID, void *usrptr, 
+                       unsigned char *TagVal)
 {
 	//turn on the Onboard LED
 	CPhidgetRFID_setLEDOn(RFID, 1);
 	
-	printf("Tag Read: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1], TagVal[2], TagVal[3], TagVal[4]);
+	printf("Tag Read: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1],
+ 		TagVal[2], TagVal[3], TagVal[4]);
 	in_RFID_savetag(TagVal[0]+TagVal[1]+TagVal[2]+TagVal[3]+TagVal[4]);
 	return 0;
 }
 
 
-int in_RFID_TagLostHandler(CPhidgetRFIDHandle RFID, void *usrptr, unsigned char *TagVal)
+int in_RFID_TagLostHandler(CPhidgetRFIDHandle RFID, void *usrptr,
+                           unsigned char *TagVal)
 {
 	//turn off the Onboard LED
 	CPhidgetRFID_setLEDOn(RFID, 0);
 
-	printf("Tag Lost: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1], TagVal[2], TagVal[3], TagVal[4]);
+	printf("Tag Lost: %02x%02x%02x%02x%02x\n", TagVal[0], TagVal[1],
+                TagVal[2], TagVal[3], TagVal[4]);
 	return 0;
 }
 
@@ -84,20 +92,22 @@ if(tagv==IN_KEYFOBTWO)
 }
 
 /*Interface Kit input*/
-int in_kit_InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int State)
+int in_kit_InputChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr,
+                              int Index, int State)
 {
 	//printf("Digital Input: %d > State: %d\n", Index, State);
 	return 0;
 }
 
-int in_kit_SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr, int Index, int Value)
+int in_kit_SensorChangeHandler(CPhidgetInterfaceKitHandle IFK, void *usrptr,
+                               int Index, int Value)
 {
 	//printf("Sensor: %d > Value: %d\n", Index, Value);
-	in_kit_laugh(Index,Value);
+	in_kit_save(Index,Value);
 	return 0;
 }
 
-void in_kit_laugh(int sindex, int svalue)
+void in_kit_save(int sindex, int svalue)
 {
 	if(svalue > IN_PRESS && sindex==IN_FORCE)
 		{
@@ -116,7 +126,7 @@ void in_kit_laugh(int sindex, int svalue)
 					last=current;
 					if(dif>1)
 						{
-							in_set_input(INPT_FORCE);//FORCE
+						in_set_input(INPT_FORCE);//FORCE
 						}
 				}
 
